e9cf3fab3f9f6d51d410ec3183514b9843038d0e
kileysel <kiley@selmanhouse.com>
2018-06-06 18:35:10 +0000

    Done.

---
+++ arrays.rb
@@ -0,0 +1,13 @@
+# student_1 = "Kiley"
+# student_2 = "Emi"
+# student_3 = "Delaney"
+# student_4 = "Alex"
+student_names = ["Kiley", "Emi"]
+#puts student_names
+#puts student_names[0]
+# to add something to an array use .push
+student_names.push("Delaney")
+student_names.push("Alex")
+student_names.push("Beyonce")
+puts student_names.size 
+puts student_names.length
