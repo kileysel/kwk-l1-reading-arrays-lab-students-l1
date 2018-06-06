03d4d9a2db6024d76c93e0ac6dd96eb48e33d3cf
kileysel <kiley@selmanhouse.com>
2018-06-06 20:48:27 +0000

    automatically backed up by learn

--- amazon_cart.rb
+++ amazon_cart.rb
@@ -2,7 +2,6 @@ cart_item_prices = [12.43, 19.89, 3.49, 75.00]
 cart_item_prices.push(5.30)
 
 # puts cart_item_prices
-
 # counts = 1 
 # cart_item_prices.each do |price|
 #   puts "the price is #{price}"
