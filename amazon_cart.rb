6b52e1694c0754a428a0bc2bfce61a7a4f147061
kileysel <kiley@selmanhouse.com>
2018-06-06 21:03:28 +0000

    automatically backed up by learn

--- amazon_cart.rb
+++ amazon_cart.rb
@@ -7,16 +7,25 @@
 #   count += 1 
 #end 
 
-cart_item_prices = [12.43, 19.89, 3.49, 75.00]
+# cart_item_prices = [12.43, 19.89, 3.49, 75.00]
 
-tax_included = []
+# tax_included = []
 
-cart_item_prices.each do |price|
-  price_with_tax = price * 1.08 
-  ##tax_included << price_with_tax
-  tax_included.push(price_with_tax)
-end
-puts tax_included
+# cart_item_prices.each do |price|
+#   price_with_tax = price * 1.08 
+#   ##tax_included << price_with_tax
+#   tax_included.push(price_with_tax)
+# end
+# puts tax_included
   
+cart_item_prices = [12.43, 19.89, 3.49, 75.00]
+ big_ticket_prices = []
+ cart_item_prices.each do |price|
+   if price >= 15 
+     big_ticket_prices << price 
+   end
+end 
+ puts big_ticket_prices
+# => [19.99, 75.00]
   
   
