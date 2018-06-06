0c307f82e8808933aea66c6757a0f1ddc1e2fa15
kileysel <kiley@selmanhouse.com>
2018-06-06 18:58:18 +0000

    Done.

--- arrays.rb
+++ arrays.rb
@@ -10,4 +10,8 @@ student_names.push("Delaney")
 student_names.push("Alex")
 student_names.push("Beyonce")
 puts student_names.size 
+<<<<<<< HEAD
 puts student_names.length
+=======
+puts student_names.length
+>>>>>>> 3bb7bebce6df4c0ebe6bd3c6340a8c69ab331aa7
--- reading_arrays.rb
+++ reading_arrays.rb
@@ -1,27 +1,38 @@
-STUDENT_NAMES = [
-  "Adele",
-  "Beyoncé",
-  "Cardi B",
-  "Lady Gaga",
-  "Nicki Minaj",
-  "Rihanna"
-]
+#student_names["Adele", "Beyoncé", "Cardi B", "Lady Gaga", "Nicki Minaj", "Rihanna"]
+#   "Adele",
+#   "Beyoncé",
+#   "Cardi B",
+#   "Lady Gaga",
+#   "Nicki Minaj",
+#   "Rihanna"
+# ]
 
 def first_student_by_index
   # Write a solution that returns the first student in the array student_names
+  student_names = ["Adele", "Beyoncé", "Cardi B", "Lady Gaga", "Nicki Minaj", "Rihanna"]
+  puts student_names[0]
 end
+first_student_by_index
+
 
 def fourth_student_by_index
   # Write a solution that returns the first student in the array student_names
+  student_names = ["Adele", "Beyoncé", "Cardi B", "Lady Gaga", "Nicki Minaj", "Rihanna"]
+  puts student_names[4]
 end
-
+fourth_student_by_index
 def last_student_by_index
   # Write a solution that returns the first student in the array student_names
+  student_names = ["Adele", "Beyoncé", "Cardi B", "Lady Gaga", "Nicki Minaj", "Rihanna"]
+  puts student_names[5]
 end
 
 def first_student_by_method
   # Write a solution that returns the first student using the built in .first method
+  student_names = ["Adele", "Beyoncé", "Cardi B", "Lady Gaga", "Nicki Minaj", "Rihanna"]
+  puts student_names[1]
 end
+first_student_by_method
 
 def last_student_by_method
   # Write a solution that returns the first student using the built in .last method
